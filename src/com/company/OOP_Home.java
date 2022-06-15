8c226d820ce19bb0153cfa7bc424515f5805ce17
zhenyka9696 <zhenyka9696@mail.ru>
2022-06-15 12:03:28 +0300

    0

--- src/com/company/OOP_Home.java
+++ src/com/company/OOP_Home.java
@@ -1,6 +1,5 @@
 package com.company;
 
-//import java.util.LinkedList;
 import java.util.*;
 
 public class OOP_Home {
@@ -16,7 +15,6 @@ public class OOP_Home {
         Player player8 = new Player("Player 8");
         Player player9 = new Player("Player 9");
         Player player10 = new Player("Player 10");
-
 
         List<Player> players = new LinkedList<>();
         players.add(player1);
@@ -58,182 +56,44 @@ public class OOP_Home {
         games.add(game10);
 
         Rate rate = new Rate(players);
-        for (Game game : games) {//used to be
+        for (Game game : games) {
             game.starter();
             rate.change();
         }
         //<-
-
-        Scanner scanner = new Scanner(System.in);
-        System.out.println("Внимание, некорректная функция!");
-        System.out.println("Введите размер таблицы общего рейтинга оппонента(x&&y)");
-        PlayerOpponent.setHome(scanner.nextInt() + 4) ;
-        System.out.println("Начальное положение общего рейтинга по успеху оппонента(0..n): ");
-        int success =PlayerOpponent.setHome(PlayerOpponent.getHome() - scanner.nextInt() - 3); // home - scanner.nextInt() - 3;
-        System.out.println("Начальное положение общего рейтинга по провалу оппонента(0..n): ");
-        int failure = scanner.nextInt() + 2;
-
-        PlayerOpponent.setEventGrid(new int[PlayerOpponent.getHome()][PlayerOpponent.getHome()]);
-        PlayerOpponent.setGeneralSuccess((PlayerOpponent.getGeneralSuccess()-4)*(PlayerOpponent.getGeneralSuccess()-4));
-
-        for (int s = 0; s < PlayerOpponent.getHome(); s++) {
-            for (int f = 0; f < PlayerOpponent.getHome(); f++) {
-                if (s < 2 || s > PlayerOpponent.getHome() - 3 || f < 2 || f > PlayerOpponent.getHome() - 3) {
-                    PlayerOpponent.setEventGrid_sf(s,f,-1);//eventGrid[s][f] = -1;
-                }
+        PlayerOpponent playerOpponent=new PlayerOpponent();
+        System.out.println(playerOpponent.opponents());
             }
         }
 
-        PlayerOpponent.setEventGrid_successfailure(success,failure,-1);//eventGrid[success][failure]=1;
-        if(PlayerOpponent.solve(success,failure,2)){
-            PlayerOpponent.printResult();
-            }
-        else System.out.println("no result");
-    }
+ class PlayerOpponent{
 
-
-}
-
-class PlayerOpponent{
-
-public static int getHome() {
-        return home;
-    }
-
-    public static int setHome(int home) {
-        PlayerOpponent.home = home;
-        return home;
-    }
-
-    public static int[][] getMoves() {
+    public  int[]getMoves() {
         return moves;
     }
 
-    public static void setMoves(int[][] moves) {
-        PlayerOpponent.moves = moves;
-    }
-
-    public static int[][] getEventGrid() {
-        return eventGrid;
-    }
-
-    public static void setEventGrid(int[][] eventGrid) {
-        PlayerOpponent.eventGrid = eventGrid;
-    }
-
-    public static int getEventGrid_sf(int s,int f){
-    return eventGrid[s][f];
-    }
-
-    public static void setEventGrid_sf(int s,int f,int eventGrid_sf){
-    eventGrid[s][f]=eventGrid_sf;
-    }
-
-    public static int getEventGrid_successfailure(int success,int failure){
-        return eventGrid[success][failure];
-    }
-
-    public static void setEventGrid_successfailure(int success,int failure,int eventGrid_successfailure){
-        eventGrid[success][failure]=eventGrid_successfailure;
-    }
-
-    public static int getGeneralSuccess() {
-        return generalSuccess;
-    }
-
-    public static void setGeneralSuccess(int generalSuccess) {
-        PlayerOpponent.generalSuccess = generalSuccess;
-    }
-
-    public PlayerOpponent(int home, int[][] moves, int[][] eventGrid, int generalSuccess) {
+    public PlayerOpponent() {
         this.moves=moves;
-        this.eventGrid=eventGrid;
-        this.generalSuccess=generalSuccess;
-        this.home=home;
     }
 
-    private static int home;
-    private static int[][] moves = {{1, -2}, {2, -1}, {2, 1}, {1, 2}, {-1, 2}, {-2, 1}, {-2, -1}, {-1, -2}}; // Possible moves by knight on chess
-    private static int[][] eventGrid;    // grid
-    private static int generalSuccess;   // total squares
-    // Return True when solvable
-    public static boolean solve(int success, int failure, int count) {
-        List<int[]> coach = coaches(success, failure);
 
-        if (coach.isEmpty() && count != generalSuccess) {
-            return false;
+    private int[] moves = {1, -2, 2, -1, 2, 1, 1, 2, -1, 2, -2, 1, -2, -1, -1, -2}; // Possible moves
+
+    public   List<String> opponents() {
+        List<String> opponents = new ArrayList<>();
+        int success=0;
+        for (int value:getMoves()){
+            success+=value;
+           }
+
+        for (int n=1;n<11;n++){
+            String number=Integer.toString(n);
+            opponents.add("Player"+number+" Rate="+(success+(int)(Math.random()*401-201)));
+            }
+
+        return opponents;
         }
 
-        Collections.sort(coach, new Comparator<int[]>() {
-            public int compare(int[] a, int[] b) {
-                return a[2] - b[2];
-            }
-        });
-
-        for (int[] aa : coach) {
-            success = aa[0];
-            failure = aa[1];
-            eventGrid[success][failure] = count;
-            //->
-            if (!semi_final(generalSuccess, success, failure) && solve(success, failure, count + 1)) {
-                return true;
-            }
-            eventGrid[success][failure] = 0;
-        }
-
-        return false;
-    }
-
-    // Returns List of coaches
-    private static List<int[]> coaches(int success, int failure) {
-        List<int[]> coach = new ArrayList<>();
-        for (int[] m : moves) {
-            int x = m[0];
-            int y = m[1];
-            if (eventGrid[success + y][failure + x] == 0) {
-                int num = countCoaches(success + y, failure + x);
-                coach.add(new int[]{success + y, failure + x, num});
-            }
-        }
-        return coach;
-    }
-
-    // Returns the generalSuccess of coaches
-    private static int countCoaches(int success, int failure) {
-        int num = 0;
-        for (int[] m : moves) {
-            if (eventGrid[success + m[1]][failure + m[0]] == 0) {
-                num++;
-            }
-        }
-        return num;
-    }
-
-    // Returns true if it is semi_final
-    private static boolean semi_final(int count, int success, int failure) {
-        if (count < generalSuccess - 1) {
-            List<int[]> coach = coaches(success, failure);
-            for (int[] aa : coach) {
-                if (countCoaches(aa[0], aa[1]) == 0) {
-                    return true;
-                }
-            }
-        }
-        return false;
-    }
-
-    // Prints the result grid
-    public static void printResult() {
-        for (int[] success : eventGrid) {
-            for (int i : success) {
-                if (i == -1) {
-                    continue;
-                }
-                System.out.printf("%2d ", i);
-            }
-            System.out.println();
-        }
-    }
 }
 
 class Game {
@@ -251,9 +111,9 @@ class Game {
         System.out.println(getGameName()+" has begun!");//->
     }
 
-    public void looking_for_game(){
+/*    public void looking_for_game(){
         System.out.println(" Looking for game: "+ getGameName());
-    }
+    }*/
 }
 class Player {
     public Player(String name){
